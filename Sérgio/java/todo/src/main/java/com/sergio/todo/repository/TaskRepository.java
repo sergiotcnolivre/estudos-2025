3430b683c17f6ead107ff6932d83db51d406ad24
Sérgio Campos <sergiotcnolivre@gmail.com>
2025-01-09 21:47:01 -0300

    feat: create task repository

---
+++ Sérgio/java/todo/src/main/java/com/sergio/todo/repository/TaskRepository.java
@@ -0,0 +1,14 @@
+package com.sergio.todo.repository;
+
+import com.sergio.todo.enums.PRIORITY;
+import com.sergio.todo.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    Task findByTitle(String title);
+
+    List<Task> findByPriority(PRIORITY priority);
+}
